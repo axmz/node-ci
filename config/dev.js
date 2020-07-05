9725ebbbec89217a0ac1a712fdc7309547404286
Alexandr Moroz <afmoroz@gmail.com>
2020-07-05 22:17:58 +0300

    reupload

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+// Don't be like me and commit this file!
+// These keys have been disabled, but remain here for reference purposes.
+module.exports = {
+  googleClientID: '1058003007205-bjtgjq5p1ascvs44hq679gl0ununr08v.apps.googleusercontent.com',
+  googleClientSecret: 'OUCQX0latkhRH4vEuI6qCLIG',
+  mongoURI: 'mongodb+srv://axmz:#TcFvsz@TH7V@ve@cluster0.tfmeq.mongodb.net/nodeci?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAIWSXFHRM7F6Z3NWQ',
+  secretAccessKey: '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'
+};
