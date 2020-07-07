1ef6e7e52ddb2650b452ebd1c848eef6758c6b33
Alex <afmoroz@gmail.com>
2020-07-08 00:31:00 +0300

    prep for ci

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,6 @@
 module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  googleClientID: '1058003007205-bjtgjq5p1ascvs44hq679gl0ununr08v.apps.googleusercontent.com',
+  googleClientSecret: 'OUCQX0latkhRH4vEuI6qCLIG',
   mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
