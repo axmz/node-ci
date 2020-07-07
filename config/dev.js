2b825e0e5b5df9ab1f4faa630295d4cc78cd4fcd
Alex <afmoroz@gmail.com>
2020-07-07 23:54:19 +0300

    google + mongo keys

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,8 @@
 module.exports = {
   googleClientID: '1058003007205-bjtgjq5p1ascvs44hq679gl0ununr08v.apps.googleusercontent.com',
   googleClientSecret: 'OUCQX0latkhRH4vEuI6qCLIG',
-  mongoURI: 'mongodb+srv://axmz:#TcFvsz@TH7V@ve@cluster0.tfmeq.mongodb.net/nodeci?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://axmz:%23TcFvsz@TH7V@ve@cluster0.tfmeq.mongodb.net/nodeci?retryWrites=true&w=majority',
+  // mongoURI: 'mongodb://axmz:#TcFvsz@TH7V@ve@cluster0-shard-00-00.tfmeq.mongodb.net:27017,cluster0-shard-00-01.tfmeq.mongodb.net:27017,cluster0-shard-00-02.tfmeq.mongodb.net:27017/nodeci?ssl=true&replicaSet=atlas-2rpwj8-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379',
   accessKeyId: 'AKIAIWSXFHRM7F6Z3NWQ',
